01d1aa00987b9409ddd9805db682c8bf2bae8d0e
Artem Beresnev <aeberesnev@avito.ru>
2023-08-15 14:43:02 +0400

    HW2: handle splash for escaping zipNumber

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,21 +2,35 @@ package hw02unpackstring
 
 import (
 	"errors"
-	"fmt"
 	"strconv"
 	"strings"
+	"unicode/utf8"
 )
 
 var ErrInvalidString = errors.New("invalid string")
+
+func isSlash(letter rune) bool {
+	slashRune, _ := utf8.DecodeRuneInString("\\")
+	if slashRune == letter {
+		return true
+	}
+	return false
+}
 
 func Unpack(line string) (string, error) {
 	runes := []rune(line)
 	unzipLetters := []rune{}
 
 	for index := 0; index < len(runes); index++ {
-		fmt.Printf("index: %d\n", index)
+		//fmt.Printf("index: %d\n", index)
+		if isSlash(runes[index]) {
+			//fmt.Print("isSplash\n")
+			unzipLetters = append(unzipLetters, runes[index+1])
+			index += 1
+			continue
+		}
 		if zipNumber, err := strconv.Atoi(string(runes[index])); err == nil {
-			fmt.Printf("zipNumber: %d\n", zipNumber)
+			//fmt.Printf("zipNumber: %d\n", zipNumber)
 			if zipNumber == 0 {
 				unzipLetters = unzipLetters[:len(unzipLetters)-1]
 			} else {
@@ -25,10 +39,10 @@ func Unpack(line string) (string, error) {
 				}
 			}
 		} else {
-			fmt.Printf("rune: %s\n", string(runes[index]))
+			//fmt.Printf("rune: %s\n", string(runes[index]))
 			unzipLetters = append(unzipLetters, runes[index])
 		}
-		fmt.Printf("collection: [%s]\n", string(unzipLetters))
+		//fmt.Printf("collection: [%s]\n", string(unzipLetters))
 	}
 
 	var b strings.Builder
