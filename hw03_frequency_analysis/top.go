e58885e7c9c410394367cf67f400bdd1334e6266
Artem Beresnev <artembert@gmail.com>
2023-08-30 22:00:39 +0400

    HW3: apply the reference solution

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -14,64 +14,32 @@ type WordCounter struct {
 	Count int
 }
 
-func getSliceFromDictionary(dictionary map[string]int) []WordCounter {
-	arr := []WordCounter{}
-
-	for key, value := range dictionary {
-		arr = append(arr, WordCounter{Word: key, Count: value})
-	}
-	return arr
-}
-
-func groupWordsByFrequency(arr []WordCounter) map[int][]string {
-	frequencyDictionary := map[int][]string{}
-
-	for _, value := range arr {
-		frequencyDictionary[value.Count] = append(frequencyDictionary[value.Count], value.Word)
-	}
-
-	return frequencyDictionary
-}
-
-func getMostFrequentWords(dictionary map[int][]string, amount int) []string {
-	sortedFrequencies := make([]int, 0, len(dictionary))
-	for frequency := range dictionary {
-		sortedFrequencies = append(sortedFrequencies, frequency)
-	}
-	sort.Slice(sortedFrequencies, func(i, j int) bool {
-		return sortedFrequencies[i] > sortedFrequencies[j]
-	})
-
-	count := amount
-	currentFrequentIndex := 0
-	result := make([]string, 0, amount)
-	for count > 0 && currentFrequentIndex < len(sortedFrequencies) {
-		wordsListByCurrentFrequency := dictionary[sortedFrequencies[currentFrequentIndex]]
-		sort.Strings(wordsListByCurrentFrequency)
-		index := 0
-		for count > 0 && index < len(wordsListByCurrentFrequency) {
-			result = append(result, wordsListByCurrentFrequency[index])
-			count--
-			index++
-		}
-		currentFrequentIndex++
-	}
-
-	return result
-}
-
 func Top10(text string) []string {
-	dictionary := map[string]int{}
+	wordsCountMap := map[string]int{}
 
 	for _, word := range delimiter.Split(text, -1) {
 		if len(word) == 0 || delimiter.MatchString(word) {
 			continue
 		}
-		dictionary[word]++
+		wordsCountMap[word]++
 	}
 
-	sliceFromDictionary := getSliceFromDictionary(dictionary)
-	wordsByFrequency := groupWordsByFrequency(sliceFromDictionary)
+	wordsSlice := make([]string, 0, len(wordsCountMap))
+	for word := range wordsCountMap {
+		wordsSlice = append(wordsSlice, word)
+	}
 
-	return getMostFrequentWords(wordsByFrequency, MostFrequentWordsAmount)
+	sort.Slice(wordsSlice, func(i, j int) bool {
+		a, b := wordsSlice[i], wordsSlice[j]
+		if wordsCountMap[a] == wordsCountMap[b] {
+			return a < b
+		} else {
+			return wordsCountMap[a] > wordsCountMap[b]
+		}
+	})
+
+	if len(wordsCountMap) > MostFrequentWordsAmount {
+		return wordsSlice[:MostFrequentWordsAmount]
+	}
+	return wordsSlice[:len(wordsCountMap)]
 }
