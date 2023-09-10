94b4be0ed436018dac254cfb9097e4093ac724d0
Artem Beresnev <aeberesnev@avito.ru>
2023-09-10 22:49:41 +0400

    HW4: declare methods of Cache

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,8 +9,6 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
@@ -22,4 +20,19 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (list *lruCache) Set(key Key, value interface{}) bool {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (list *lruCache) Get(key Key) (interface{}, bool) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (list *lruCache) Clear() {
+	//TODO implement me
+	panic("implement me")
 }
