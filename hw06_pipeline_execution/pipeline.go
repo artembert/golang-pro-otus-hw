289ca809ab855643da1d9afe27d717930987106e
Artem Beresnev <artembert@gmail.com>
2023-10-07 15:45:53 +0400

    HW6: implement ExecutePipeline func

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,35 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
+	for _, stage := range stages {
+		in = stage(stream(done, in))
+	}
 	return nil
+}
+
+func stream(done In, in In) Out {
+	out := make(Bi)
+
+	go func() {
+		defer close(out)
+
+		for {
+			select {
+			case data, ok := <-in:
+				if !ok {
+					return
+				}
+
+				select {
+				case out <- data:
+				case <-done:
+					return
+				}
+			case <-done:
+				return
+			}
+		}
+	}()
+
+	return out
 }
