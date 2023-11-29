db1952f3a2e01048409abc03f7fc933fe09618e7
Artem Beresnev <artembert@gmail.com>
2023-11-29 22:33:12 +0400

    HW7: implement Copy()

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -3,35 +3,76 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io"
 	"os"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
+	ErrFileRead              = errors.New("failed to read file")
+	ErrFileWrite             = errors.New("failed to write to file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrFileDoesNotExist      = errors.New("file does not exist")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
-	return nil
-
-}
-
-func openFile(path string) (*os.File, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		if os.IsNotExist(err) {
-			fmt.Println("file does not exist")
-		} else {
-			fmt.Println("error opening file:", err)
-		}
-	}
+	sourceFile, err := openFile(fromPath, offset)
 	defer func(file *os.File) {
 		err := file.Close()
 		if err != nil {
 			fmt.Println("error closing file:", err)
 		}
-	}(file)
+	}(sourceFile)
+	if sourceFile == nil {
+		return err
+	}
+
+	if err != nil {
+		fmt.Println("error seeking file:", err)
+		return ErrOffsetExceedsFileSize
+	}
+
+	var buffer []byte = nil
+	if limit != 0 {
+		buffer = make([]byte, limit)
+	}
+
+	_, err = io.ReadFull(sourceFile, buffer)
+	if err != nil {
+		fmt.Println("error reading file:", err)
+		return ErrFileRead
+	}
+
+	distFile, err := createFile(toPath)
+	if err != nil {
+		fmt.Println("error creating file:", err)
+		return err
+	}
+
+	_, err = io.CopyBuffer(distFile, sourceFile, buffer)
+	if err != nil {
+		fmt.Println("error writing to file:", err)
+		return ErrFileWrite
+	}
+
+	err = distFile.Close()
+	if err != nil {
+		fmt.Println("error closing file:", err)
+		return err
+	}
+	return nil
+}
+
+func openFile(path string, offset int64) (*os.File, error) {
+	file, err := os.Open(path)
+	_, err = file.Seek(offset, io.SeekStart)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, ErrFileDoesNotExist
+		} else {
+			return nil, ErrUnsupportedFile
+		}
+	}
 
 	return file, err
 }
