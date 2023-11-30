4a7049a162deeb20cad654cbb577ba0c9b3b0235
Artem Beresnev <artembert@gmail.com>
2023-11-30 13:26:54 +0400

    HW7: extract reusable functions

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -7,21 +7,30 @@ import (
 	"testing"
 )
 
+func dropTempFolder(t *testing.T, path string) {
+	err := os.RemoveAll(path)
+	if err != nil {
+		t.Errorf("Error removing test folder '%v': %v", path, err)
+	}
+}
+
+func prepareTestDir(t *testing.T) (string, string) {
+	dir := filepath.Join("tmp", strings.Replace(t.Name(), "/", "_", -1))
+	err := os.MkdirAll(dir, os.FileMode(0700))
+	if err != nil {
+		t.Errorf("Error creating test directory: %v", err)
+	}
+	outFile := filepath.Join(dir, "out.txt")
+
+	return dir, outFile
+}
+
 func TestCopy(t *testing.T) {
 	t.Run("Offset: 0, Limit: 0", func(t *testing.T) {
-		dir := filepath.Join("tmp", strings.Replace(t.Name(), "/", "_", -1))
-		err := os.MkdirAll(dir, os.FileMode(0700))
-		if err != nil {
-			t.Errorf("Error creating test directory: %v", err)
-		}
-		defer func(path string) {
-			err := os.RemoveAll(path)
-			if err != nil {
-				t.Errorf("Error removing test folder '%v': %v", dir, err)
-			}
-		}(dir)
-		outFile := filepath.Join(dir, "out.txt")
-		err = Copy("testdata/input.txt", outFile, 0, 0)
+		dir, outFile := prepareTestDir(t)
+		defer dropTempFolder(t, dir)
+
+		err := Copy("testdata/input.txt", outFile, 0, 0)
 		if err != nil {
 			t.Errorf("Expected nil, got %v", err)
 		}
