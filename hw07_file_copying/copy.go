d30ee4a2c2219b8ab46a85f25c2ec8a7db41fb1d
Artem Beresnev <artembert@gmail.com>
2023-11-30 14:08:07 +0400

    HW7: simplify Copy fn

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -9,16 +9,19 @@ import (
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
-	ErrFileRead              = errors.New("failed to read file")
 	ErrFileWrite             = errors.New("failed to write to file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 	ErrFileDoesNotExist      = errors.New("file does not exist")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	sourceFile, err := openFile(fromPath, offset)
+	sourceFile, err := os.Open(fromPath)
 	if err != nil {
-		return err
+		if os.IsNotExist(err) {
+			return ErrFileDoesNotExist
+		} else {
+			return ErrUnsupportedFile
+		}
 	}
 	defer func(file *os.File) {
 		err := file.Close()
@@ -26,60 +29,41 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 			fmt.Println("error closing file:", err)
 		}
 	}(sourceFile)
-	if sourceFile == nil {
-		fmt.Printf("error reading file '%v'\n", fromPath)
-		return ErrFileRead
+
+	if offset > 0 {
+		fileInfo, err := sourceFile.Stat()
+		if err != nil {
+			return err
+		}
+		if offset > fileInfo.Size() {
+			return ErrOffsetExceedsFileSize
+		}
+	}
+
+	_, err = sourceFile.Seek(offset, io.SeekStart)
+	if err != nil {
+		return err
 	}
 
 	distFile, err := os.Create(toPath)
 	if err != nil {
-		fmt.Println("error creating file:", err)
 		return err
 	}
-
-	if limit == 0 {
-		_, err = io.Copy(distFile, sourceFile)
-	} else {
-		_, err = io.CopyN(distFile, sourceFile, limit)
-	}
-	if err != nil {
-		if err != io.EOF {
-			fmt.Println("error writing to file:", err)
-			return ErrFileWrite
-		}
-	}
-
 	defer func(file *os.File) {
 		err := file.Close()
 		if err != nil {
 			fmt.Println("error closing file:", err)
 		}
 	}(distFile)
+
+	if limit == 0 {
+		_, err = io.Copy(distFile, sourceFile)
+	} else {
+		_, err = io.CopyN(distFile, sourceFile, limit)
+	}
+	if err != nil && err != io.EOF {
+		return ErrFileWrite
+	}
+
 	return nil
-}
-
-func openFile(path string, offset int64) (*os.File, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return nil, ErrFileDoesNotExist
-		} else {
-			return nil, ErrUnsupportedFile
-		}
-	}
-	if offset > 0 {
-		fileInfo, err := file.Stat()
-		if err != nil {
-			return nil, err
-		}
-		if offset > fileInfo.Size() {
-			return nil, ErrOffsetExceedsFileSize
-		}
-	}
-	_, err = file.Seek(offset, io.SeekStart)
-	if err != nil {
-		return nil, err
-	}
-
-	return file, err
 }
