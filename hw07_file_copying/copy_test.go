28cece693c093b25989bc81e170b1b3e0c2d0b98
Artem Beresnev <artembert@gmail.com>
2023-11-30 13:19:23 +0400

    HW7: unit test

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,29 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("Offset: 0, Limit: 0", func(t *testing.T) {
+		dir := filepath.Join("tmp", strings.Replace(t.Name(), "/", "_", -1))
+		err := os.MkdirAll(dir, os.FileMode(0700))
+		if err != nil {
+			t.Errorf("Error creating test directory: %v", err)
+		}
+		defer func(path string) {
+			err := os.RemoveAll(path)
+			if err != nil {
+				t.Errorf("Error removing test folder '%v': %v", dir, err)
+			}
+		}(dir)
+		outFile := filepath.Join(dir, "out.txt")
+		err = Copy("testdata/input.txt", outFile, 0, 0)
+		if err != nil {
+			t.Errorf("Expected nil, got %v", err)
+		}
+	})
 }
