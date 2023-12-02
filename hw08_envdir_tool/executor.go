fa5942be9213843db11a64959812fde7dfeb3267
Artem Beresnev <artembert@gmail.com>
2023-12-03 00:00:13 +0400

    HW8: pass variables from folder to env

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"os/exec"
 )
 
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
+	processEnvs(env)
 	command := exec.Command(cmd[0])
 	command.Env = append(os.Environ(), ToPairsSlice(env)...)
 
@@ -28,4 +30,20 @@ func RunCmd(cmd []string, env Environment) (returnCode int) {
 		panic(err)
 	}
 	return
+}
+
+func processEnvs(env Environment) {
+	for key, value := range env {
+		if value.NeedRemove {
+			err := os.Unsetenv(key)
+			if err != nil {
+				fmt.Println("error while unsetting env: ", err)
+			}
+		} else {
+			err := os.Setenv(key, value.Value)
+			if err != nil {
+				fmt.Printf("error while setting env by name [%s]: %s\n", key, err)
+			}
+		}
+	}
 }
