3c6e926063f2167a2a8b05594bb3e9ea2464708f
Artem Beresnev <artembert@gmail.com>
2023-12-05 00:09:53 +0400

    HW8: RunCmd: unit-test

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -3,5 +3,10 @@ package main
 import "testing"
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("Should pass error code from child process", func(t *testing.T) {
+		returnCode := RunCmd([]string{"./testdata/throw_error_code.sh"}, nil)
+		if returnCode != 42 {
+			t.Errorf("Expected return code 42, got %v", returnCode)
+		}
+	})
 }
