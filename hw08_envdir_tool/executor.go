af8eaca3005db1dc9a743d0271bcd376a59f359f
Artem Beresnev <artembert@gmail.com>
2023-12-05 00:31:57 +0400

    HW8: extract SetupEnv to dedicated folder

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -9,9 +9,8 @@ import (
 
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	processEnvs(env)
+	SetupEnv(env)
 	command := exec.Command(cmd[0], cmd[1:]...) // #nosec G204
-	command.Env = append(os.Environ(), ToPairsSlice(env)...)
 
 	command.Stdout = os.Stdout
 	command.Stdin = os.Stdin
@@ -32,20 +31,4 @@ func RunCmd(cmd []string, env Environment) (returnCode int) {
 		return -1
 	}
 	return
-}
-
-func processEnvs(env Environment) {
-	for key, value := range env {
-		if value.NeedRemove {
-			err := os.Unsetenv(key)
-			if err != nil {
-				fmt.Println("error while unsetting env: ", err)
-			}
-		} else {
-			err := os.Setenv(key, value.Value)
-			if err != nil {
-				fmt.Printf("error while setting env by name [%s]: %s\n", key, err)
-			}
-		}
-	}
 }
