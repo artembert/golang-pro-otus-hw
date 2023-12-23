d146cc5ec21c23416a954dbe9d20a967fc6f9394
Artem Beresnev <artembert@gmail.com>
2023-12-23 21:15:00 +0400

    HW9: Validate: support validation of []string

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -2,6 +2,7 @@ package hw09structvalidator
 
 import (
 	"errors"
+	"fmt"
 	"reflect"
 )
 
@@ -35,6 +36,13 @@ func Validate(v interface{}) error {
 				return parsingError
 			}
 			fieldErrors = validationResult
+
+			if fieldErrors != nil {
+				validationErrors = append(validationErrors, ValidationError{
+					Field: field.Name,
+					Err:   errors.Join(fieldErrors...),
+				})
+			}
 		case reflect.Int:
 			val := int(valueOfStruct.Field(i).Int())
 			validationResult, parsingError := ValidateInt(val, rule)
@@ -42,14 +50,34 @@ func Validate(v interface{}) error {
 				return parsingError
 			}
 			fieldErrors = validationResult
-		case reflect.Slice:
-		}
 
-		if fieldErrors != nil {
-			validationErrors = append(validationErrors, ValidationError{
-				Field: field.Name,
-				Err:   errors.Join(fieldErrors...),
-			})
+			if fieldErrors != nil {
+				validationErrors = append(validationErrors, ValidationError{
+					Field: field.Name,
+					Err:   errors.Join(fieldErrors...),
+				})
+			}
+		case reflect.Slice:
+			sliceKind := field.Type.Elem().Kind()
+			slice := valueOfStruct.Field(i)
+
+			switch sliceKind {
+			case reflect.String:
+				for sliceElIndex := 0; sliceElIndex < valueOfStruct.Field(i).Len(); sliceElIndex++ {
+					val := slice.Index(sliceElIndex).String()
+					validationResult, parsingError := ValidateString(val, rule)
+					if parsingError != nil {
+						return parsingError
+					}
+					if validationResult != nil {
+						validationErrors = append(validationErrors, ValidationError{
+							Field: fmt.Sprintf("%s[%s]", field.Name, val),
+							Err:   errors.Join(validationResult...),
+						})
+					}
+				}
+				//case reflect.Int:
+			}
 		}
 	}
 
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -39,6 +39,12 @@ type (
 		WorkingEmail string `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
 	}
 
+	DemographicsAndContacts struct {
+		Phones        []string `validate:"len:11"`
+		WorkingPhones []string `validate:"len:11"`
+		BirthDate     []int    `validate:"min:0|max:2040"`
+	}
+
 	Token struct {
 		Header    []byte
 		Payload   []byte
@@ -113,6 +119,18 @@ func TestValidate(t *testing.T) {
 			},
 			expectedErr: ValidationErrors{
 				ValidationError{Field: "WorkingEmail", Err: ErrRegexp{Constraint: "regexp:^\\w+@\\w+\\.\\w+$", GivenValue: "test-admin@corp"}},
+			}.Error(),
+		},
+		{
+			name: "Slice validation",
+			examine: DemographicsAndContacts{
+				Phones:        []string{"54-555-123", "554555-123"},
+				WorkingPhones: []string{"554-555-123", "554-555-122", "554-555-123"},
+				BirthDate:     []int{-30, 10, 20, 40, 2050},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "Phones[54-555-123]", Err: ErrLengthConstraint{Constraint: 11, GivenValue: "54-555-123"}},
+				ValidationError{Field: "Phones[554555-123]", Err: ErrLengthConstraint{Constraint: 11, GivenValue: "554555-123"}},
 			}.Error(),
 		},
 	}
