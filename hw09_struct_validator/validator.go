cda3aa3b1dda8e0a20ddab63430d395c9d0eafa1
Artem Beresnev <artembert@gmail.com>
2023-12-24 00:06:21 +0400

    HW9: Validate: simplify complexity of

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -61,37 +61,12 @@ func Validate(v interface{}) error {
 		case reflect.Slice:
 			sliceKind := field.Type.Elem().Kind()
 			slice := valueOfStruct.Field(i)
-
-			switch sliceKind {
-			case reflect.String:
-				for sliceElIndex := 0; sliceElIndex < valueOfStruct.Field(i).Len(); sliceElIndex++ {
-					val := slice.Index(sliceElIndex).String()
-					validationResult, parsingError := ValidateString(val, rule)
-					if parsingError != nil {
-						return parsingError
-					}
-					if validationResult != nil {
-						validationErrors = append(validationErrors, ValidationError{
-							Field: fmt.Sprintf("%s[%s]", field.Name, val),
-							Err:   errors.Join(validationResult...),
-						})
-					}
-				}
-			case reflect.Int:
-				for sliceElIndex := 0; sliceElIndex < valueOfStruct.Field(i).Len(); sliceElIndex++ {
-					val := int(slice.Index(sliceElIndex).Int())
-					validationResult, parsingError := ValidateInt(val, rule)
-					if parsingError != nil {
-						return parsingError
-					}
-					if validationResult != nil {
-						validationErrors = append(validationErrors, ValidationError{
-							Field: fmt.Sprintf("%s[%s]", field.Name, strconv.FormatInt(int64(val), 10)),
-							Err:   errors.Join(validationResult...),
-						})
-					}
-				}
-			default:
+			sliceValidationErrors, parsingError := validateSlice(slice, sliceKind, rule, field.Name)
+			if parsingError != nil {
+				return parsingError
+			}
+			if sliceValidationErrors != nil {
+				validationErrors = append(validationErrors, sliceValidationErrors...)
 			}
 		default:
 		}
@@ -102,4 +77,48 @@ func Validate(v interface{}) error {
 	}
 
 	return nil
+}
+
+func validateSlice(
+	slice reflect.Value,
+	sliceKind reflect.Kind,
+	rule string,
+	fieldName string,
+) (validationErrors []ValidationError, parsingError error) {
+	switch sliceKind {
+	case reflect.String:
+		for sliceElIndex := 0; sliceElIndex < slice.Len(); sliceElIndex++ {
+			val := slice.Index(sliceElIndex).String()
+			validationResult, parsingError := ValidateString(val, rule)
+			if parsingError != nil {
+				return nil, parsingError
+			}
+			if validationResult != nil {
+				validationErrors = append(validationErrors, ValidationError{
+					Field: fmt.Sprintf("%s[%s]", fieldName, val),
+					Err:   errors.Join(validationResult...),
+				})
+			}
+		}
+
+		return validationErrors, nil
+	case reflect.Int:
+		for sliceElIndex := 0; sliceElIndex < slice.Len(); sliceElIndex++ {
+			val := int(slice.Index(sliceElIndex).Int())
+			validationResult, parsingError := ValidateInt(val, rule)
+			if parsingError != nil {
+				return nil, parsingError
+			}
+			if validationResult != nil {
+				validationErrors = append(validationErrors, ValidationError{
+					Field: fmt.Sprintf("%s[%s]", fieldName, strconv.FormatInt(int64(val), 10)),
+					Err:   errors.Join(validationResult...),
+				})
+			}
+		}
+
+		return validationErrors, nil
+	default:
+		return nil, nil
+	}
 }
