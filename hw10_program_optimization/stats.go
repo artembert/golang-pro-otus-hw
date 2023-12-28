94533ae9401b4eab50dfd5630845b1f02bfbd1a5
Artem Beresnev <artembert@gmail.com>
2023-12-29 00:29:48 +0400

    HW10: iterate over source file line-by-line

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,10 +1,16 @@
 package hw10programoptimization
 
 import (
+	"bufio"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"strings"
+)
+
+var (
+	ErrUserParsing = errors.New("user parsing failed")
 )
 
 type User struct {
@@ -17,46 +23,29 @@ type User struct {
 	Address  string
 }
 
+type Users [100_000]User
+
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := GetUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return CountDomains(u, domain)
-}
-
-type Users [100_000]User
-
-func GetUsers(r io.Reader) (result Users, err error) {
-	content, err := io.ReadAll(r)
-	if err != nil {
-		return
-	}
-
-	lines := strings.Split(string(content), "\n")
-	user := &User{}
-	for i, line := range lines {
-		err = json.Unmarshal([]byte(line), user)
-		if err != nil {
-			return
-		}
-		result[i] = *user
-	}
-	return result, nil
-}
-
-func CountDomains(u Users, domain string) (DomainStat, error) {
 	result := make(DomainStat)
-	query := "." + domain
 
-	for _, user := range u {
+	fileScanner := bufio.NewScanner(r)
+	fileScanner.Split(bufio.ScanLines)
+	query := "." + domain
+	user := &User{}
+
+	for fileScanner.Scan() {
+		err := json.Unmarshal(fileScanner.Bytes(), user)
+		if err != nil {
+			return nil, fmt.Errorf("%w, %v", ErrUserParsing, err)
+		}
 		matched := strings.Contains(user.Email, query)
 		if matched {
 			key := strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])
 			result[key]++
 		}
 	}
+
 	return result, nil
 }
