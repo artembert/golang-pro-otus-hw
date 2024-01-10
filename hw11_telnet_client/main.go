5268c8bf72ce593cfbc996ed0ad34ab2402b13de
Artem Beresnev <artembert@gmail.com>
2024-01-11 01:26:15 +0400

    HW9: implement wrapper for telnet

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,75 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const TIMEOUT = 10 * time.Second
+
+var (
+	ErrWrongArgs = errors.New("hostname and port required")
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	if err := run(); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func run() error {
+	timeout := flag.Duration("timeout", TIMEOUT, "connection timeout")
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) < 2 || len(args[0]) == 0 || len(args[1]) == 0 {
+		return ErrWrongArgs
+	}
+
+	address := net.JoinHostPort(args[0], args[1])
+	telnet := NewTelnetClient(address, *timeout, os.Stdin, os.Stdout)
+	if err := telnet.Connect(); err != nil {
+		return err
+	}
+	defer telnet.Close()
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT)
+	defer cancel()
+
+	go send(&telnet, cancel)
+	go receive(&telnet, cancel)
+
+	<-ctx.Done()
+
+	err := telnet.Close()
+	return err
+}
+
+func send(tc *TelnetClient, cancelFunc context.CancelFunc) {
+	if err := (*tc).Send(); err != nil {
+		_, _1 := fmt.Fprintf(os.Stderr, "%v\n", err)
+		_ = _1
+	} else {
+		_, _1 := fmt.Fprintf(os.Stderr, "diconnect.\n")
+		_ = _1
+	}
+	cancelFunc()
+}
+
+func receive(tc *TelnetClient, cancelFunc context.CancelFunc) {
+	if err := (*tc).Receive(); err != nil {
+		_, _1 := fmt.Fprintf(os.Stderr, "%v\n", err)
+		_ = _1
+	} else {
+		_, _1 := fmt.Fprintf(os.Stderr, "Connection have been closed by peer.\n")
+		_ = _1
+	}
+	cancelFunc()
 }
