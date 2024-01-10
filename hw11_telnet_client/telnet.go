bb870603f40ae2f7acac586dca2c22d3f21042eb
Artem Beresnev <artembert@gmail.com>
2024-01-10 23:59:51 +0400

    HW9: implement telnet

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"errors"
 	"io"
+	"net"
 	"time"
+)
+
+var (
+	ErrConnection              = errors.New("connection error")
+	ErrNoEstablishedConnection = errors.New("no established connection")
+	ErrCloseConnections        = errors.New("failed to close connection")
+	ErrSend                    = errors.New("failed to send message")
+	ErrReceive                 = errors.New("failed to receive message")
 )
 
 type TelnetClient interface {
@@ -12,10 +22,66 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type telnetClient struct {
+	address    string
+	timeout    time.Duration
+	connection net.Conn
+	in         io.ReadCloser
+	out        io.Writer
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &telnetClient{
+		address:    address,
+		timeout:    timeout,
+		in:         in,
+		out:        out,
+		connection: nil,
+	}
+}
+
+func (tc *telnetClient) Connect() error {
+	connection, err := net.DialTimeout("tcp", tc.address, tc.timeout)
+	if err != nil {
+		return errors.Join(err, ErrConnection)
+	}
+
+	tc.connection = connection
+
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (tc *telnetClient) Close() error {
+	if tc.connection == nil {
+		return ErrNoEstablishedConnection
+	}
+
+	err := tc.connection.Close()
+	if err != nil {
+		return errors.Join(err, ErrCloseConnections)
+	}
+
+	return nil
+}
+
+func (tc *telnetClient) Send() error {
+	if tc.connection == nil {
+		return ErrNoEstablishedConnection
+	}
+
+	_, err := io.Copy(tc.connection, tc.in)
+	if err != nil {
+		return errors.Join(err, ErrSend)
+	}
+
+	return nil
+}
+
+func (tc *telnetClient) Receive() error {
+	_, err := io.Copy(tc.out, tc.connection)
+	if err != nil {
+		return errors.Join(err, ErrReceive)
+	}
+
+	return nil
+}
