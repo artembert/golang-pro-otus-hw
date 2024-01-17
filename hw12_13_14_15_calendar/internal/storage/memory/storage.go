d2214ab9335ee4b93a33b875ea34c0cb210e0a9d
Artem Beresnev <artembert@gmail.com>
2024-01-18 01:01:39 +0400

    HW10: declare methods in MemoryStorage

--- hw12_13_14_15_calendar/internal/storage/memory/storage.go
+++ hw12_13_14_15_calendar/internal/storage/memory/storage.go
@@ -1,14 +1,46 @@
 package memorystorage
 
-import "sync"
+import (
+	"github.com/artembert/golang-pro-otus-hw/hw12_13_14_15_calendar/internal/storage"
+	"sync"
+)
+
+type eventsCollection map[string]storage.Event
+
+type Logger interface {
+	Error(msg string)
+}
 
 type Storage struct {
 	// TODO
-	mu sync.RWMutex //nolint:unused
+	mu     sync.RWMutex //nolint:unused
+	events eventsCollection
+	log    Logger
 }
 
-func New() *Storage {
-	return &Storage{}
+func New(log Logger) *Storage {
+	return &Storage{
+		events: make(eventsCollection),
+		log:    log,
+	}
 }
 
-// TODO
+func (s *Storage) CreateEvent(evt storage.Event) error {
+	return nil
+}
+
+func (s *Storage) DeleteEvent(evt storage.Event) error {
+	return nil
+}
+
+func (s *Storage) UpdateEvent(evt storage.Event) error {
+	return nil
+}
+
+func (s *Storage) GetEventByID(id string) error {
+	return nil
+}
+
+func (s *Storage) GetAllEvents() ([]storage.Event, error) {
+	return make([]storage.Event, 0), nil
+}
