1a3336794180ecc24e5e5257a8bd96d80fb49644
Artem Beresnev <artembert@gmail.com>
2024-01-21 20:54:13 +0400

    HW12: loggerzap: add test

--- hw12_13_14_15_calendar/go.mod
+++ hw12_13_14_15_calendar/go.mod
@@ -5,11 +5,13 @@ go 1.19
 require (
 	github.com/jackc/pgx/v4 v4.18.1
 	github.com/pkg/errors v0.8.1
+	github.com/stretchr/testify v1.8.1
 	go.uber.org/zap v1.26.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.14.0 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -18,6 +20,7 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/jackc/puddle v1.3.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
--- hw12_13_14_15_calendar/internal/pkg/loggerzap/loggerzap_test.go
+++ hw12_13_14_15_calendar/internal/pkg/loggerzap/loggerzap_test.go
@@ -1,7 +1,68 @@
 package loggerzap
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/artembert/golang-pro-otus-hw/hw12_13_14_15_calendar/internal/interfaces/loglevel"
+	"github.com/stretchr/testify/require"
+)
 
 func TestLogger(t *testing.T) {
-	// TODO
+	cases := []struct {
+		message  string
+		logLevel loglevel.LogLevel
+	}{
+		{
+			message:  "Test Info Message",
+			logLevel: loglevel.Info,
+		},
+		{
+			message:  "Something goes wrong",
+			logLevel: loglevel.Error,
+		},
+		{
+			message:  "Deprecated function called",
+			logLevel: loglevel.Warn,
+		},
+		{
+			message:  "Connection established",
+			logLevel: loglevel.Debug,
+		},
+	}
+	for _, testCase := range cases {
+		t.Run(string(testCase.logLevel), func(t *testing.T) {
+			tmpfile, err := os.CreateTemp("", "log")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer func() {
+				_ = os.Remove(tmpfile.Name())
+			}()
+
+			logger, err := New(loglevel.Debug, tmpfile.Name())
+			if err != nil {
+				t.Fatal(err)
+			}
+			switch testCase.logLevel {
+			case loglevel.Info:
+				logger.Info(testCase.message)
+			case loglevel.Error:
+				logger.Error(testCase.message)
+			case loglevel.Debug:
+				logger.Debug(testCase.message)
+			case loglevel.Warn:
+				logger.Warn(testCase.message)
+			}
+
+			content, err := os.ReadFile(tmpfile.Name())
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			require.Contains(t, string(content), testCase.message)
+			require.Contains(t, string(content), fmt.Sprintf("\"level\":\"%s\"", testCase.logLevel))
+		})
+	}
 }
