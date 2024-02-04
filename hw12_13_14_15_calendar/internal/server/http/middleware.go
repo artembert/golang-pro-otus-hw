4b81f33e2057b1ef270e17b1184f5efcd3ba60a9
Artem Beresnev <artembert@gmail.com>
2024-02-04 22:33:28 +0400

    HW13: update loggingMiddleware

--- hw12_13_14_15_calendar/internal/server/http/middleware.go
+++ hw12_13_14_15_calendar/internal/server/http/middleware.go
@@ -12,20 +12,21 @@ func loggingMiddleware(logger logger.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			startTime := time.Now()
-			next.ServeHTTP(w, r)
+			defer func() {
+				logger.HTTPRequest(
+					r,
+					fmt.Sprintf(
+						"%s %s %s %v %s",
+						r.Method,
+						r.URL.Path,
+						r.Proto,
+						time.Since(startTime),
+						r.UserAgent(),
+					),
+				)
+			}()
 
-			logger.HTTPRequest(
-				r,
-				fmt.Sprintf(
-					"%s %s %s %d %v %s",
-					r.Method,
-					r.URL.Path,
-					r.Proto,
-					http.StatusOK,
-					time.Since(startTime),
-					r.UserAgent(),
-				),
-			)
+			next.ServeHTTP(w, r)
 		})
 	}
 }
